e7f87504b530d836150601241dc3ed92453b319d
DoanGiaBao <baodoangia2003@gmail.com>
2023-09-29 14:12:47 +0700

    lap2

--- SachOnline/Controllers/SachOnlineController.cs
+++ SachOnline/Controllers/SachOnlineController.cs
@@ -1,25 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration.Provider;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SachOnline.Models;
 
 namespace SachOnline.Controllers
 {
     public class SachOnlineController : Controller
     {
+        private string connection;
+        private dbSachOnlineDataContext data;
+        private List<SACH> LaySachMoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.SachID).Take(count).ToList();
+        }
+        public SachOnlineController()
+        {
+            // Khởi tạo chuỗi kết nối
+            connection = "Data Source=LAPTOP-O5R6LPBI;Initial Catalog=SachOnline;Integrated Security=True";
+            data = new dbSachOnlineDataContext(connection);
+        }
+
+
         // GET: SachOnline
         public ActionResult Index()
         {
-            return View();
+            //Lay 6 quyen sach moi
+            var listSachMoi = LaySachMoi(6);
+            return View(listSachMoi);
         }
-        public ActionResult ChuDePartial()
+       /* public ActionResult ChuDePartial()
+        {
+            return PartialView();
+        }*/
+        public ActionResult NhaXuatBanPartial()
         {
             return PartialView();
         }
-        public ActionResult NXBPartial()
-        {
-            return PartialView();
-        }
+        public ActionResult ChuDePartial() { 
+            var listChuDe = from cd in data.CHUDEs select cd; 
+            return PartialView(listChuDe); }
     }
 }
